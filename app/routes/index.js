8934f385a15e7dbd94952d0579e0e2b2545448c2
Frankie Gavin <frankanthonygavin@gmail.com>
2017-01-29 19:51:16 -0800

    minor changes

--- app/routes/index.js
+++ app/routes/index.js
@@ -13,7 +13,7 @@ export default Ember.Route.extend({
       var newQuestion = this.store.createRecord('question', params);
       newQuestion.save();
       this.transitionTo('index');
-    }
+    },
 
 
   }

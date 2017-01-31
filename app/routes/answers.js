ed65c4b8b6a250e64f5ce91c6c6c79647be39c7f
Frankie Gavin <frankanthonygavin@gmail.com>
2017-01-30 21:28:45 -0800

    questions and answers are properly saving to Firebase and displaying to the site.

--- app/routes/answers.js
+++ app/routes/answers.js
@@ -2,14 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
    actions: {
-  saveAnswer(params) {
-    var newAnswer = this.store.createRecord('answer', params);
-    var question = params.question;
-    question.get('answers').addObject(newAnswer);
-    newAnswer.save().then(function() {
-      return question.save();
-    });
-    this.transitionTo('question', question);
-  }
+
 }
 });
